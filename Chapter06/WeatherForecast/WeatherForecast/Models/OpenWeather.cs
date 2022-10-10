b415d1cc114cbe9825e9bddf2869443640b8aba8
Toi B Wright <toi@onestopdesigns.com>
2022-10-09 22:46:18 -0500

    Chapter 6

---
+++ Chapter06/WeatherForecast/WeatherForecast/Models/OpenWeather.cs
@@ -0,0 +1,24 @@
+﻿namespace WeatherForecast.Models
+{
+    public class OpenWeather
+    {
+        public Daily[] Daily { get; set; }
+    }
+    public class Daily
+    {
+        public long Dt { get; set; }
+        public Temp Temp { get; set; }
+        public Weather[] Weather { get; set; }
+    }
+    public class Temp
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+    }
+    public class Weather
+    {
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+}
---
+++ Chapter06/WeatherForecast/WeatherForecast/Models/Position.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherForecast.Models
+{
+    public class Position
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
