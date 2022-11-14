2c2c2006f2cd2b980b53146ae453e9fcc23fd727
Toi B Wright <toi@onestopdesigns.com>
2022-11-13 21:20:24 -0600

    Chapter 9

---
+++ Chapter09/ExcelReader/ExcelReader/Models/Medals.cs
@@ -0,0 +1,11 @@
+﻿namespace ExcelReader.Models
+{
+    public class Medals
+    {
+        public int Year { get; set; }
+        public string? Country { get; set; }
+        public int Gold { get; set; }
+        public int Silver { get; set; }
+        public int Bronze { get; set; }
+    }
+}
