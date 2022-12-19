9c24b20268d388488043944aeb5ac416cd372c47
Toi B Wright <toi@onestopdesigns.com>
2022-12-18 20:20:52 -0600

    Chapter 12

---
+++ Chapter12/ExpenseTracker/ExpenseTracker/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ExpenseTracker.Server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ExpenseTrackerServerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ExpenseTrackerServerContext") ?? throw new InvalidOperationException("Connection string 'ExpenseTrackerServerContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
---
+++ Chapter12/ExpenseTracker/ExpenseTracker/Shared/ExpenseType.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpenseTracker.Shared
+{
+    public class ExpenseType
+    {
+        public int Id { get; set; }
+        public string? Type { get; set; }
+    }
+}
